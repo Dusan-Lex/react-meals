341d70c1556197d140be9aabdbfc31cbf471b3ad
Dusan-Lex <dusanlexmilosavljevic@gmail.com>
2021-05-14 21:42:23 +0200

    handling loading state and errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,12 +6,17 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-meals-3ae3d-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
       const data = await response.json();
       let mealsArr = [];
       for (let i in data) {
@@ -23,19 +28,36 @@ const AvailableMeals = () => {
         });
       }
       setMeals(mealsArr);
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
   }, []);
 
-  const mealsList = meals.map((meal) => <MealItem key={meal.id} {...meal} />);
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>Loading...</section>;
+  }
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+  if (error) {
+    return <section className={classes.MealsError}>{error}</section>;
+  }
+  if (meals.length !== 0) {
+    return (
+      <section className={classes.meals}>
+        <Card>
+          <ul>
+            {meals.map((meal) => (
+              <MealItem key={meal.id} {...meal} />
+            ))}
+          </ul>
+        </Card>
+      </section>
+    );
+  }
+  return null;
 };
 
 export default AvailableMeals;
